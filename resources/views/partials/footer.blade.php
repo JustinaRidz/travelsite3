9548b8a854f30e6a1fdf35b6c129670bfb20ff7c
Justina <justina.ridz@gmail.com>
2020-09-11 23:09:09 +0300

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer style="width: 100%; height: 50px; background-color: #bbb5; position: fixed; bottom: 0">
+    <h1>Footer</h1>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Trečias bandymas</h1>
+@endsection
